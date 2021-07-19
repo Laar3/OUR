77afb26a66aaef39b41d0ba42006dc30c20efd76
MrVeink <joona.holkko@tuta.io>
2021-07-19 14:34:18 +0300

    Added first package for OPK

---
+++ repo/pfetch/build.sh
@@ -0,0 +1,22 @@
+pkgname=pfetch
+pkgver=master
+
+fetch() {
+	curl -L "https://github.com/dylanaraps/pfetch/archive/refs/heads/master.zip" -o $pkgname-$pkgver.zip
+	unzip $pkgname-$pkgver.zip
+}
+
+build() {
+	cd $pkgname-$pkgver
+	echo "Nothing to do"
+}
+
+package() {
+	cd $pkgname-$pkgver
+	install -Dm755 pfetch /usr/bin/
+}
+
+license() {
+	cd $pkgname-$pkgver
+	cat LICENSE.md
+}
