0e931c25c4fe8adc3cd425990bfb0df5dfa3d4fa
Laar3 <Laar@tutanota.com>
2021-10-17 20:34:27 +1030

    Added Byacc build script

---
+++ repo/byacc/build.sh
@@ -0,0 +1,25 @@
+pkgname=byacc
+pkgver=20210808
+LICENSE=LICENSE
+
+fetch() {
+        curl -L "https://invisible-island.net/datafiles/release/byacc.tar.gz" -o $pkgname-$pkgver.tar.gz
+	tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+        cd $pkgname-$pkgver
+        ./configure --prefix=/usr
+        make
+}
+
+package() {
+        cd $pkgname-$pkgver
+        make install
+}
+
+license() {
+        cd $pkgname-$pkgver
+        cat $LICENSE
+}
+
