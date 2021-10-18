303c25c63316a8f736fe984aab615146e1927536
Laar3 <Laar@tutanota.com>
2021-10-18 13:56:40 +1030

    Added zlib build script

---
+++ repo/zlib/build.sh
@@ -0,0 +1,24 @@
+pkgname=zlib
+pkgver=1.2.11
+LICENSE=README
+
+fetch() {
+        curl -l "https://www.zlib.net/zlib-1.2.11.tar.gz" -o $pkgname-$pkgver.tar.gz
+        tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+        cd $pkgname-$pkgver
+        ./configure
+        make
+}
+
+package() {
+        cd $pkgname-$pkgver
+        make install
+}
+
+license() {
+        cd $pkgname-$pkgver
+        cat $LICENSE
+}
