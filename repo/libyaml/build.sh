5169af26d9f5fd845680447e57c411068ded16e0
Laar3 <Laar@tutanota.com>
2021-10-18 14:37:19 +1030

    added libyaml

---
+++ repo/libyaml/build.sh
@@ -0,0 +1,24 @@
+pkgname=yaml
+pkgver=0.2.5
+LICENSE=License
+
+fetch() {
+        curl -L "http://pyyaml.org/download/libyaml/yaml-0.2.5.tar.gz" -o $pkgname-$pkgver.tar.gz
+        tar -xf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+        cd $pkgname-$pkgver
+        ./configure
+        make
+}
+
+package() {
+        cd $pkgname-$pkgver
+        make install
+}
+
+license() {
+        cd $pkgname-$pkgver
+        cat $LICENSE
+}
